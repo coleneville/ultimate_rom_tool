36c4a0b6ee843e1116c75067aecd1a6258f1ff61
Cole Neville <colewneville2@gmail.com>
2021-10-02 15:34:15 -0600

    Starting to create some install/setup tooling

---
+++ setup.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'ultimate_rom_tool',
+  version = '1.0',
+)
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+export PYTHON_SYSTEM_RUNTIME=python
+
+$PYTHON_SYSTEM_RUNTIME -m venv .venv
+
+if test -f '.venv/scripts/activate'; then
+  source .venv/Scripts/activate
+else
+  source .venv/bin/activate
+fi
+
+pip install -r requirements.txt
+pip install -e .
+
+echo \
+'TO_SORT=../to_sort
+ROM_ROOT=../rom_root
+DAT_ROOT=../dat_root' > .env
